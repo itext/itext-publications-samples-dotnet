bf8b662b874a700c2c145489cb64644821ba53c0
Yulian Gaponenko <yulian.gaponenko@duallab.com>
2016-09-30 18:58:30 +0300

    Add missing WrapToTest attribute
    DEVSIX-865

--- itext/itext.publications/itext.publications.highlevel/itext/highlevel/chapter05/C05E01_MyFirstTable.cs
+++ itext/itext.publications/itext.publications.highlevel/itext/highlevel/chapter05/C05E01_MyFirstTable.cs
@@ -7,9 +7,11 @@ using System.IO;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
+using iText.Test.Attributes;
 
 namespace iText.Highlevel.Chapter05 {
     /// <author>Bruno Lowagie (iText Software)</author>
+    [WrapToTest]
     public class C05E01_MyFirstTable {
         public const String DEST = "../../results/chapter05/my_first_table.pdf";
 
