be230d2c3729850ac9111ad140edf40175e6c025
iText Software <development@itextpdf.com>
2018-07-27 15:38:52 +0300

    [RELEASE] 7.1.3-SNAPSHOT -> 7.1.3

--- itext/itext.publications/itext.publications.signatures/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.signatures/Properties/AssemblyInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("7.1.3.0")]
 [assembly: AssemblyFileVersion("7.1.3.0")]
-[assembly: AssemblyInformationalVersion("7.1.3-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("7.1.3")]
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
