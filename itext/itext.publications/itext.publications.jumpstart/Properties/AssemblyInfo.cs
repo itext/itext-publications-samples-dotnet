3ae9f8345a4080a88ce9aac9b71b3fbad6a04ea9
iText Software <development@itextpdf.com>
2019-04-24 12:22:17 +0300

    [RELEASE] 7.1.6-SNAPSHOT -> 7.1.7-SNAPSHOT

--- itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("eee84512-f6a8-4cd0-946b-6513d33fdea1")]
 
-[assembly: AssemblyVersion("7.1.6.0")]
-[assembly: AssemblyFileVersion("7.1.6.0")]
-[assembly: AssemblyInformationalVersion("7.1.6-SNAPSHOT")]
+[assembly: AssemblyVersion("7.1.7.0")]
+[assembly: AssemblyFileVersion("7.1.7.0")]
+[assembly: AssemblyInformationalVersion("7.1.7-SNAPSHOT")]
