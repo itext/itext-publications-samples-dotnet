43b70b77e2a395b1b8b3b3170bbc60e53a1286de
Yauheni Borbut <yauheni.borbut@duallab.com>
2019-10-10 11:36:01 +0300

    Port Barcodes samples
    DEVSIX-3341

---
+++ itext/itext.samples/itext/samples/sandbox/barcodes/BarcodeBackground.cs
@@ -0,0 +1,59 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2019 iText Group NV
+Authors: iText Software.
+
+For more information, please contact iText Software at this address:
+sales@itextpdf.com
+*/
+
+using System;
+using System.IO;
+using iText.Barcodes;
+using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Xobject;
+
+namespace iText.Samples.Sandbox.Barcodes
+{
+    public class BarcodeBackground
+    {
+        public static readonly String DEST = "../../results/sandbox/barcodes/barcode_background.pdf";
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new BarcodeBackground().ManipulatePdf(DEST);
+        }
+
+        protected void ManipulatePdf(String dest)
+        {
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+
+            Barcode128 code128 = new Barcode128(pdfDoc);
+            code128.SetCode("12345XX789XXX");
+            code128.SetCodeType(Barcode128.CODE128);
+            PdfFormXObject xObject = code128.CreateFormXObject(ColorConstants.BLACK,
+                ColorConstants.BLACK, pdfDoc);
+
+            float x = 36;
+            float y = 750;
+            float width = xObject.GetWidth();
+            float height = xObject.GetHeight();
+
+            // Draw the rectangle with set background color and add the created barcode object.
+            PdfCanvas canvas = new PdfCanvas(pdfDoc.AddNewPage());
+            canvas.SaveState();
+            canvas.SetFillColor(ColorConstants.LIGHT_GRAY);
+            canvas.Rectangle(x, y, width, height);
+            canvas.Fill();
+            canvas.RestoreState();
+            canvas.AddXObject(xObject, 36, 750);
+
+            pdfDoc.Close();
+        }
+    }
+}
---
+++ itext/itext.samples/itext/samples/sandbox/barcodes/BarcodePlacement.cs
@@ -0,0 +1,65 @@
+/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2019 iText Group NV
+Authors: iText Software.
+
+For more information, please contact iText Software at this address:
+sales@itextpdf.com
+*/
+
+using System;
+using System.IO;
+using iText.Barcodes;
+using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Xobject;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace iText.Samples.Sandbox.Barcodes
+{
+    public class BarcodePlacement
+    {
+        public static readonly String DEST = "../../results/sandbox/barcodes/barcode_placement.pdf";
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new BarcodePlacement().ManipulatePdf(DEST);
+        }
+
+        public Image CreateBarcode(float xScale, float yScale, PdfDocument pdfDoc)
+        {
+            BarcodePDF417 barcode = new BarcodePDF417();
+            barcode.SetCode("BarcodePDF417 barcode");
+            PdfFormXObject barcodeObject = barcode.CreateFormXObject(ColorConstants.BLACK, pdfDoc);
+            Image barcodeImage = new Image(barcodeObject).Scale(xScale, yScale);
+            return barcodeImage;
+        }
+
+        protected void ManipulatePdf(String dest)
+        {
+            PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+            Document doc = new Document(pdfDoc);
+
+            Image img = CreateBarcode(1, 1, pdfDoc);
+            doc.Add(new Paragraph(String.Format("This barcode measures {0:0.0} by {1:0.0} user units",
+                img.GetImageScaledWidth(), img.GetImageScaledHeight())));
+            doc.Add(img);
+
+            img = CreateBarcode(3, 3, pdfDoc);
+            doc.Add(new Paragraph(String.Format("This barcode measures {0:0.0} by {1:0.0} user units",
+                img.GetImageScaledWidth(), img.GetImageScaledHeight())));
+            doc.Add(img);
+
+            img = CreateBarcode(3, 1, pdfDoc);
+            doc.Add(new Paragraph(String.Format("This barcode measures {0:0.0} by {1:0.0} user units",
+                img.GetImageScaledWidth(), img.GetImageScaledHeight())));
+            doc.Add(img);
+
+            doc.Close();
+        }
+    }
+}
