a3e2f8127edc23d10efa6b6cd53082a0e1d3f929
Yauheni Borbut <yauheni.borbut@duallab.com>
2019-12-16 17:05:59 +0300

    Port Velocity2Pdf sample
    DEVSIX-3531

---
+++ itext/itext.samples/itext/samples/sandbox/pdfhtml/Velocity2Pdf.cs
@@ -0,0 +1,53 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2019 iText Group NV
+    Authors: iText Software.
+
+    For more information, please contact iText Software at this address:
+    sales@itextpdf.com
+ */
+
+using System;
+using System.IO;
+using iText.Html2pdf;
+using NVelocity;
+using NVelocity.App;
+
+namespace iText.Samples.Sandbox.Pdfhtml
+{
+    public class Velocity2Pdf
+    {
+        public static readonly String DEST = "results/sandbox/pdfhtml/velocity-test.pdf";
+
+        public static readonly String SRC = "../../resources/pdfhtml/templates/velocity-test.vm";
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new Velocity2Pdf().ManipulatePdf(DEST);
+        }
+
+        protected void ManipulatePdf(String dest)
+        {
+            // Initialize the velocity engine
+            VelocityEngine engine = new VelocityEngine();
+            engine.Init();
+
+            // Create a velocity context and populate it
+            VelocityContext context = new VelocityContext();
+            context.Put("message", "Hello World!");
+
+            // Load the template
+            StringWriter writer = new StringWriter();
+            Template template = engine.GetTemplate(SRC);
+            template.Merge(context, writer);
+
+            using (FileStream stream = new FileStream(dest, FileMode.Create))
+            {
+                HtmlConverter.ConvertToPdf(writer.ToString(), stream);
+            }
+        }
+    }
+}
