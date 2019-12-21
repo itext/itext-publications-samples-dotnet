72aa601a4f5951dc6cfdf333ca06eee6883f823e
Alex Rasolko <alex.rasolko@duallab.com>
2019-12-21 16:44:49 +0300

    Refactor SimpleTable12 sandbox sample
    DEVSIX-3426

--- itext/itext.samples/itext/samples/sandbox/tables/SimpleTable12.cs
+++ itext/itext.samples/itext/samples/sandbox/tables/SimpleTable12.cs
@@ -7,9 +7,13 @@ For more information, please contact iText Software at this address:
 sales@itextpdf.com
 */
 
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/31263533/how-to-create-nested-column-using-itextsharp
+ */
+
 using System;
 using System.IO;
-using iText.IO.Font.Constants;
 using iText.Kernel.Font;
 using iText.Kernel.Geom;
 using iText.Kernel.Pdf;
@@ -24,8 +28,6 @@ namespace iText.Samples.Sandbox.Tables
     {
         public static readonly string DEST = "results/sandbox/tables/simple_table12.pdf";
 
-        private static PdfFont font;
-
         public static void Main(String[] args)
         {
             FileInfo file = new FileInfo(DEST);
@@ -39,61 +41,55 @@ namespace iText.Samples.Sandbox.Tables
             PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
             Document doc = new Document(pdfDoc, PageSize.A4.Rotate());
 
-            font = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
-
             Table table = new Table(UnitValue.CreatePercentArray(8)).UseAllAvailableWidth();
             
-            table.AddCell(CreateCell("Examination", 1, 2, 15));
-            table.AddCell(CreateCell("Board", 1, 2, 15));
-            table.AddCell(CreateCell("Month and Year of Passing", 1, 2, 15));
-            table.AddCell(CreateCell("", 1, 1, 1));
-            table.AddCell(CreateCell("Marks", 2, 1, 1));
-            table.AddCell(CreateCell("Percentage", 1, 2, 15));
-            table.AddCell(CreateCell("Class / Grade", 1, 2, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
-            table.AddCell(CreateCell("Obtained", 1, 1, 15));
-            table.AddCell(CreateCell("Out of", 1, 1, 15));
-            table.AddCell(CreateCell("12th / I.B. Diploma", 1, 1, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
-            table.AddCell(CreateCell("Aggregate (all subjects)", 1, 1, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
-            table.AddCell(CreateCell("", 1, 1, 15));
+            // The Style instance will be passed by default always until the moment we desire to specify some custom border
+            Style style = new Style();
+            
+            // Default border width value is 0.5, we set it on 1 for cells borders of the table which will
+            // use the Style instance
+            style.SetBorder(new SolidBorder(1));
+            
+            table.AddCell(CreateCell("Examination", 1, 2, style));
+            table.AddCell(CreateCell("Board", 1, 2, style));
+            table.AddCell(CreateCell("Month and Year of Passing", 1, 2, style));
+            table.AddCell(CreateCell("", 1, 1, new Style()
+                    .SetBorder(Border.NO_BORDER)
+                    .SetBorderTop(new SolidBorder(1))
+                    .SetBorderBottom(new SolidBorder(1))
+                    .SetBorderLeft(new SolidBorder(1))));
+            table.AddCell(CreateCell("Marks", 2, 1, new Style()
+                    .SetBorder(Border.NO_BORDER)
+                    .SetBorderTop(new SolidBorder(1))
+                    .SetBorderBottom(new SolidBorder(1))
+                    .SetBorderRight(new SolidBorder(1))));
+            table.AddCell(CreateCell("Percentage", 1, 2, style));
+            table.AddCell(CreateCell("Class / Grade", 1, 2, style));
+            table.AddCell(CreateCell("", 1, 1, new Style()));
+            table.AddCell(CreateCell("Obtained", 1, 1, style));
+            table.AddCell(CreateCell("Out of", 1, 1, style));
+            table.AddCell(CreateCell("12th / I.B. Diploma", 1, 1, style));
+            table.AddCell(CreateCell("", 1, 1, style));
+            table.AddCell(CreateCell("", 1, 1, style));
+            table.AddCell(CreateCell("Aggregate (all subjects)", 1, 1, style));
+            table.AddCell(CreateCell("", 1, 1, style));
+            table.AddCell(CreateCell("", 1, 1, style));
+            table.AddCell(CreateCell("", 1, 1, style));
+            table.AddCell(CreateCell("", 1, 1, style));
 
             doc.Add(table);
 
             doc.Close();
         }
 
-        private static Cell CreateCell(String content, int colspan, int rowspan, int border)
+        private static Cell CreateCell(string content, int colspan, int rowspan, Style style) 
         {
-            Cell cell = new Cell(rowspan, colspan).Add(new Paragraph(content).SetFont(font).SetFontSize(10));
-            cell
-                .SetBorder(Border.NO_BORDER)
+            Paragraph paragraph = new Paragraph(content)
+                .SetFontSize(10)
                 .SetTextAlignment(TextAlignment.CENTER);
-
-            if (8 == (border & 8))
-            {
-                cell.SetBorderRight(new SolidBorder(1));
-                cell.SetBorderBottom(new SolidBorder(1));
-            }
-
-            if (4 == (border & 4))
-            {
-                cell.SetBorderLeft(new SolidBorder(1));
-            }
-
-            if (2 == (border & 2))
-            {
-                cell.SetBorderBottom(new SolidBorder(1));
-            }
-
-            if (1 == (border & 1))
-            {
-                cell.SetBorderTop(new SolidBorder(1));
-            }
+            
+            Cell cell = new Cell(rowspan, colspan).Add(paragraph);
+            cell.AddStyle(style);
             
             return cell;
         }
