a0381f65380e96adee553068e9a8a928e83a951b
Alex Rasolko <alex.rasolko@duallab.com>
2020-01-03 16:54:24 +0300

    Add, update copyright headers
    DEVSIX-3607

--- itext/itext.samples/itext/samples/PdfHtmlResponsiveSampleTest.cs
+++ itext/itext.samples/itext/samples/PdfHtmlResponsiveSampleTest.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/*
+This file is part of the iText (R) project.
+Copyright (c) 1998-2020 iText Group NV
+Authors: iText Software.
+
+For more information, please contact iText Software at this address:
+sales@itextpdf.com
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
