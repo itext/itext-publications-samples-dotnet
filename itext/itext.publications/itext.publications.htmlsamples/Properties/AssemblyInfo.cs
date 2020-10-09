99d8f531830d0761ff5b9a528cf21d02aee6ec7e
iText Software <development@itextpdf.com>
2020-10-09 10:12:23 +0300

    [RELEASE] Update dependency versions

--- itext/itext.publications/itext.publications.htmlsamples/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.htmlsamples/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("68C80008-E363-466A-A5D3-1B870AB9B6D0")]
 
-[assembly: AssemblyVersion("7.1.13.0")]
-[assembly: AssemblyFileVersion("7.1.13.0")]
-[assembly: AssemblyInformationalVersion("7.1.13-SNAPSHOT")]
+[assembly: AssemblyVersion("7.1.14.0")]
+[assembly: AssemblyFileVersion("7.1.14.0")]
+[assembly: AssemblyInformationalVersion("7.1.14-SNAPSHOT")]
--- itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("eee84512-f6a8-4cd0-946b-6513d33fdea1")]
 
-[assembly: AssemblyVersion("7.1.13.0")]
-[assembly: AssemblyFileVersion("7.1.13.0")]
-[assembly: AssemblyInformationalVersion("7.1.13-SNAPSHOT")]
+[assembly: AssemblyVersion("7.1.14.0")]
+[assembly: AssemblyFileVersion("7.1.14.0")]
+[assembly: AssemblyInformationalVersion("7.1.14-SNAPSHOT")]
 
 #if !NETSTANDARD1_6
 [assembly: NUnit.Framework.Timeout(60000)]
