95cb6e1a4b7dba34f439a1f4ab5c635934854a4e
iText Software <development@itextpdf.com>
2021-03-29 13:26:54 +0300

    [RELEASE] Update dependency versions

--- itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("eee84512-f6a8-4cd0-946b-6513d33fdea1")]
 
-[assembly: AssemblyVersion("7.1.15.0")]
-[assembly: AssemblyFileVersion("7.1.15.0")]
-[assembly: AssemblyInformationalVersion("7.1.15-SNAPSHOT")]
+[assembly: AssemblyVersion("7.1.16.0")]
+[assembly: AssemblyFileVersion("7.1.16.0")]
+[assembly: AssemblyInformationalVersion("7.1.16-SNAPSHOT")]
 
 #if !NETSTANDARD2_0
 [assembly: NUnit.Framework.Timeout(60000)]
