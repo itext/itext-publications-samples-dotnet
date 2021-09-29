5ae5cff71e0004d53e8a8eddddabc20699fc8dc2
iText Software <development@itextpdf.com>
2021-09-29 13:45:42 +0300

    [RELEASE] Update dependency versions

--- itext/itext.publications/itext.publications.htmlsamples/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.htmlsamples/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("68C80008-E363-466A-A5D3-1B870AB9B6D0")]
 
-[assembly: AssemblyVersion("7.2.0.0")]
-[assembly: AssemblyFileVersion("7.2.0.0")]
-[assembly: AssemblyInformationalVersion("7.2.0-SNAPSHOT")]
+[assembly: AssemblyVersion("7.2.1.0")]
+[assembly: AssemblyFileVersion("7.2.1.0")]
+[assembly: AssemblyInformationalVersion("7.2.1-SNAPSHOT")]
