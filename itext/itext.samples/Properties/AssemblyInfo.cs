e30a6baad90cb6b8144d926c43d87b2b44eda0d0
iText Software <development@itextpdf.com>
2022-06-16 16:54:37 +0000

    [AFTER RELEASE] 7.2.3

--- itext/itext.samples/Properties/AssemblyInfo.cs
+++ itext/itext.samples/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("c0e65b6f-d2e0-4ae3-bac2-0cd8c9f58a45")]
 
-[assembly: AssemblyVersion("7.2.3.0")]
-[assembly: AssemblyFileVersion("7.2.3.0")]
-[assembly: AssemblyInformationalVersion("7.2.3-SNAPSHOT")]
+[assembly: AssemblyVersion("7.2.4.0")]
+[assembly: AssemblyFileVersion("7.2.4.0")]
+[assembly: AssemblyInformationalVersion("7.2.4-SNAPSHOT")]
