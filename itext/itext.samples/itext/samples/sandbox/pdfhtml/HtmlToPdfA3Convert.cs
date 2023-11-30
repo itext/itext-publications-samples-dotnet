b8095e35176c89fca3ec84208820a16baaab6369
Nanou Persoons <nanou.persoons@apryse.com>
2023-11-30 07:37:45 +0100

    Add html2PdfA samples Remove Hello.html htmlToPdfAConvert sample
    DEVSIX-7861

---
+++ itext/itext.samples/itext/samples/sandbox/pdfhtml/HtmlToPdfA3Convert.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using iText.Html2pdf;
+using iText.Html2pdf.Resolver.Font;
+using iText.Kernel.Pdf;
+using static iText.Kernel.Pdf.PdfAConformanceLevel;
+
+namespace iText.Samples.Sandbox.Pdfhtml 
+{
+    public class HtmlToPdfA3Convert 
+    {
+        public static readonly string SRC = "../../../resources/pdfhtml/HtmlToPdfA3Convert/";
+        public static readonly string DEST = "results/sandbox/pdfhtml/HtmlToPdfA3Convert.pdf";
+
+        public static void Main(String[] args) 
+        {
+            var file = new FileInfo(DEST);
+            file.Directory.Create();
+            
+            new HtmlToPdfA3Convert().ManipulatePdf(DEST);
+        }
+
+        public void ManipulatePdf(String pdfDest) 
+        {
+            var htmlSource = SRC + "MixedContent.html";
+            
+            var inputStream = new FileStream(SRC + "sRGB Color Space Profile.icm", FileMode.Open, FileAccess.Read);
+            
+            var converterProperties = new ConverterProperties();
+            converterProperties.SetBaseUri(SRC);
+            converterProperties.SetPdfAConformanceLevel(PDF_A_3B);
+            converterProperties.SetDocumentOutputIntent(new PdfOutputIntent("Custom", "", "http://www.color.org",
+            "sRGB IEC61966-2.1", inputStream));
+            converterProperties.SetFontProvider(new DefaultFontProvider(false, true, false));
+            
+            HtmlConverter.ConvertToPdf(new FileStream(htmlSource, FileMode.Open, FileAccess.Read), new FileStream(pdfDest, FileMode.Create), converterProperties);
+        }
+    }
+}
