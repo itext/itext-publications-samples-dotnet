e36faf7b38f247d4d55cedea541ec651e7297e77
Dmitry Radchuk <dmitry.radchuk@apryse.com>
2024-02-01 12:28:06 +0300

    Add pdf/ua-2 examples
    DEVSIX-7902

---
+++ itext/itext.samples/itext/samples/sandbox/pdfua/PdfUA2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Kernel.XMP;
+using iText.Layout.Element;
+using iText.Layout;
+using iText.Kernel.Exceptions;
+
+namespace iText.Samples.Sandbox.Pdfua
+{
+    public class PdfUA2 {
+        public const String DEST = "./target/sandbox/pdfua2/pdf_ua.pdf";
+
+        public const String FONT = "./src/main/resources/font/FreeSans.ttf";
+
+        public const String UA_XMP = "./src/main/resources/xml/pdf_ua_xmp.xmp";
+
+        public static void Main(String[] args) {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+            new PdfUA2().ManipulatePdf(DEST);
+        }
+
+        public virtual void ManipulatePdf(String dest) {
+            try {
+                using (PdfDocument pdfDocument = new PdfDocument(new PdfWriter(dest, new WriterProperties().SetPdfVersion(
+                    PdfVersion.PDF_2_0)))) {
+                    Document document = new Document(pdfDocument);
+                    byte[] bytes = File.ReadAllBytes(System.IO.Path.Combine(UA_XMP));
+                    XMPMeta xmpMeta = XMPMetaFactory.Parse(new MemoryStream(bytes));
+                    pdfDocument.SetXmpMetadata(xmpMeta);
+                    //TAGGED PDF
+                    //Make document tagged
+                    pdfDocument.SetTagged();
+                    pdfDocument.GetCatalog().SetViewerPreferences(new PdfViewerPreferences().SetDisplayDocTitle(true));
+                    pdfDocument.GetCatalog().SetLang(new PdfString("en-US"));
+                    PdfDocumentInfo info = pdfDocument.GetDocumentInfo();
+                    info.SetTitle("PdfUA2 Title");
+                    //PDF/UA
+                    //Embed font
+                    PdfFont font = PdfFontFactory.CreateFont(FONT, "WinAnsi", PdfFontFactory.EmbeddingStrategy.FORCE_EMBEDDED);
+                    Paragraph paragraph = new Paragraph("Hello PdfUA2").SetFont(font);
+                    byte[] byteMetaData = pdfDocument.GetXmpMetadata();
+                    //PDF/UA
+                    //Get string xmp metadata representation
+                    String documentMetaData = iText.Commons.Utils.JavaUtil.GetStringForBytes(byteMetaData);
+                    document.Add(paragraph);
+                }
+            }
+            catch (XMPException e) {
+                //process xmp exception
+            }
+            catch (System.IO.IOException e) {
+                //process io exception
+            }
+        }
+    }
+}
