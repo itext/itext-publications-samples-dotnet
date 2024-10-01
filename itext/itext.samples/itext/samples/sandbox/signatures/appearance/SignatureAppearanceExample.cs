579623153e3894f8b375b643a1074dcc6d445a67
Nanou Persoons <nanou.persoons@apryse.com>
2024-10-01 09:39:26 +0200

    Add signature appearance samples
    DEVSIX-8587

---
+++ itext/itext.samples/itext/samples/sandbox/signatures/appearance/SignatureAppearanceExample.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using iText.Commons.Utils;
+using iText.Forms.Form.Element;
+using iText.Html2pdf.Resolver.Font;
+using iText.IO.Font.Constants;
+using iText.IO.Image;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Xobject;
+using iText.Layout.Borders;
+using iText.Layout.Properties;
+using iText.Samples.Sandbox.Signatures.Utils;
+using iText.Kernel.Crypto;
+using iText.Signatures;
+
+namespace iText.Samples.Sandbox.Signatures.Appearance
+{
+    /// <summary>Basic example of the signature appearance customizing during the document signing
+    /// using the SignatureFieldAppearance class.</summary>
+    public class SignatureAppearanceExample
+    {
+        public static readonly String DEST = "results/sandbox/signatures/appearance/signatureAppearanceExample.pdf";
+        public static readonly String SRC = "../../../resources/pdfs/signExample.pdf";
+
+        private static readonly string CERT_PATH = "../../../resources/cert/sign.pem";
+        private static readonly string IMAGE_PATH = "../../../resources/img/itext.png";
+        private static readonly string BACKGROUND_PATH = "../../../resources/img/sign.jpg";
+        
+        private static char[] PASSWORD = "testpassphrase".ToCharArray();
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new SignatureAppearanceExample().SignDocument();
+        }
+
+        protected void SignDocument()
+        {
+            var privateKey = PemFileHelper.ReadFirstKey(CERT_PATH, PASSWORD);
+            var chain = PemFileHelper.ReadFirstChain(CERT_PATH);
+
+            PdfSigner signer = new PdfSigner(new PdfReader(SRC),
+                FileUtil.GetFileOutputStream(SignatureAppearanceExample.DEST),
+                new StampingProperties());
+            
+            ImageData clientSignatureImage = ImageDataFactory.Create(IMAGE_PATH);
+            ImageData backgroundImage = ImageDataFactory.Create(BACKGROUND_PATH);
+            
+            // Set up the appearance
+            SignatureFieldAppearance appearance = new SignatureFieldAppearance(SignerProperties.IGNORED_ID)
+                // Use any of the setContent overloads to specify the content. If new SignedAppearanceText() is used,
+                // it will be filled in automatically during the signing, but could be also filled in manually using
+                // setters, e.g. new SignedAppearanceText().SetReasonLine("[Reason]: TestReason").SetLocationLine(null)
+                .SetContent("Signature appearance description", clientSignatureImage)
+                .SetBackgroundImage(ApplyBackgroundImage(backgroundImage, -1))
+                .SetBorder(new SolidBorder(new DeviceRgb(1, 92, 135), 3))
+                .SetFontColor(new DeviceRgb(252, 173, 47))
+                .SetFontSize(30);
+            
+            // Use setFont in order to specify the font, e.g. appearance.setFont(PdfFontFactory.createFont());
+            appearance.SetFontFamily(StandardFontFamilies.HELVETICA)
+                .SetProperty(Property.FONT_PROVIDER, new DefaultFontProvider());
+            
+            // Set signer properties
+            SignerProperties signerProperties = new SignerProperties()
+                .SetFieldName("signature")
+                .SetCertificationLevel(AccessPermissions.NO_CHANGES_PERMITTED)
+                .SetClaimedSignDate(DateTimeUtil.GetCurrentTime())
+                .SetPageNumber(1)
+                .SetPageRect(new Rectangle(50, 450, 200, 200))
+                .SetContact("Contact")
+                .SetSignatureCreator("Creator")
+                .SetLocation("TestCity")
+                .SetReason("TestReason")
+                .SetSignatureAppearance(appearance);
+            
+            signer.SetSignerProperties(signerProperties);
+
+            IExternalSignature pks = new PrivateKeySignature(privateKey, DigestAlgorithms.SHA256);
+            signer.SignDetached(new BouncyCastleDigest(), pks, chain, null, null, null, 
+                0, PdfSigner.CryptoStandard.CMS);
+        }
+
+        private BackgroundImage ApplyBackgroundImage(ImageData image, float imageScale)
+        {
+            BackgroundRepeat repeat = new BackgroundRepeat(BackgroundRepeat.BackgroundRepeatValue.NO_REPEAT);
+            BackgroundPosition position = new BackgroundPosition()
+                .SetPositionX(BackgroundPosition.PositionX.CENTER)
+                .SetPositionY(BackgroundPosition.PositionY.CENTER);
+            BackgroundSize size = new BackgroundSize();
+            float EPS = 1e-5f;
+            if (Math.Abs(imageScale) < EPS)
+            {
+                size.SetBackgroundSizeToValues(UnitValue.CreatePercentValue(100),
+                    UnitValue.CreatePercentValue(100));
+            }
+            else
+            {
+                if (imageScale < 0)
+                {
+                    size.SetBackgroundSizeToContain();
+                }
+                else
+                {
+                    size.SetBackgroundSizeToValues(
+                        UnitValue.CreatePointValue(imageScale * image.GetWidth()),
+                        UnitValue.CreatePointValue(imageScale * image.GetHeight()));
+                }
+            }
+
+            return new BackgroundImage.Builder()
+                .SetImage(new PdfImageXObject(image))
+                .SetBackgroundSize(size)
+                .SetBackgroundRepeat(repeat)
+                .SetBackgroundPosition(position)
+                .Build();
+        }
+    }
+}
---
+++ itext/itext.samples/itext/samples/sandbox/signatures/appearance/SignatureAppearanceLayersExample.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.IO;
+using iText.Commons.Bouncycastle.Cert;
+using iText.Commons.Bouncycastle.Crypto;
+using iText.Commons.Utils;
+using iText.Kernel.Colors;
+using iText.Kernel.Geom;
+using iText.Kernel.Exceptions;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Kernel.Pdf.Xobject;
+using iText.Samples.Sandbox.Signatures.Utils;
+using iText.Kernel.Crypto;
+using iText.Signatures;
+
+namespace iText.Samples.Sandbox.Signatures.Appearance
+{
+    /// <summary>Basic example of the signature appearance customizing during the document signing
+    /// using the SignatureFieldAppearance class and layers.</summary>
+    public class SignatureAppearanceLayersExample
+    {
+        private static char[] PASSWORD = "testpassphrase".ToCharArray();
+
+        public static readonly String DEST =
+            "results/sandbox/signatures/appearance/signatureAppearanceLayersExample.pdf";
+
+        public static readonly String SRC = "../../../resources/pdfs/signExample.pdf";
+
+        private static readonly String CERT_PATH = "../../../resources/cert/sign.pem";
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new SignatureAppearanceLayersExample().SignDocumentSignature(DEST);
+        }
+
+        protected void SignDocumentSignature(String filePath)
+        {
+            var privateKey = GetPrivateKey();
+            var chain = GetCertificateChain();
+
+            PdfSigner signer = new PdfSigner(new PdfReader(SRC), FileUtil.GetFileOutputStream(filePath),
+                new StampingProperties());
+
+            SignerProperties signerProperties = new SignerProperties()
+                .SetFieldName("signature")
+                .SetPageRect(new Rectangle(250, 500, 100, 100))
+                .SetReason("Test 1")
+                .SetLocation("TestCity");
+            signer.SetSignerProperties(signerProperties);
+
+            Rectangle rectangle = new Rectangle(0, 0, 100, 100);
+
+            PdfFormXObject layer0 = new PdfFormXObject(rectangle);
+            // Draw pink rectangle with blue border
+            new PdfCanvas(layer0, signer.GetDocument())
+                .SaveState()
+                .SetFillColor(ColorConstants.PINK)
+                .SetStrokeColor(ColorConstants.BLUE)
+                .Rectangle(0, 0, 100, 100)
+                .FillStroke()
+                .RestoreState();
+
+            PdfFormXObject layer2 = new PdfFormXObject(rectangle);
+            // Draw yellow circle with gray border
+            new PdfCanvas(layer2, signer.GetDocument())
+                .SaveState()
+                .SetFillColor(ColorConstants.YELLOW)
+                .SetStrokeColor(ColorConstants.DARK_GRAY)
+                .Circle(50, 50, 50)
+                .FillStroke()
+                .RestoreState();
+
+            signer.GetSignatureField().SetBackgroundLayer(layer0).SetSignatureAppearanceLayer(layer2);
+
+            // Sign the document using the detached mode, CMS or CAdES equivalent.
+            // This method closes the underlying pdf document, so the instance
+            // of PdfSigner cannot be used after this method call
+            IExternalSignature pks = new PrivateKeySignature(privateKey, DigestAlgorithms.SHA256);
+            signer.SignDetached(new BouncyCastleDigest(), pks, chain, null, null, null,
+                0, PdfSigner.CryptoStandard.CMS);
+        }
+
+        /**
+         * Creates signing chain for the sample. This chain shouldn't be used for the real signing.
+         *
+         * @return the chain of certificates to be used for the signing operation.
+         */
+        protected IX509Certificate[] GetCertificateChain()
+        {
+            try
+            {
+                return PemFileHelper.ReadFirstChain(CERT_PATH);
+            }
+            catch (Exception e)
+            {
+                throw new PdfException(e);
+            }
+        }
+
+        /**
+         * Creates private key for the sample. This key shouldn't be used for the real signing.
+         *
+         * @return {@link PrivateKey} instance to be used for the main signing operation.
+         */
+        protected IPrivateKey GetPrivateKey()
+        {
+            try
+            {
+                return PemFileHelper.ReadFirstKey(CERT_PATH, PASSWORD);
+            }
+            catch (Exception e)
+            {
+                throw new PdfException(e);
+            }
+        }
+    }
+}
