66fd100e2ea8a2c1a95d2b38676bc6b46d455862
Apryse Software <development@apryse.com>
2025-05-06 19:26:26 +0000

    [RELEASE] 9.2.0

--- itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
+++ itext/itext.publications/itext.publications.jumpstart/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("9.2.0.0")]
 [assembly: AssemblyFileVersion("9.2.0.0")]
-[assembly: AssemblyInformationalVersion("9.2.0-SNAPSHOT")]
+[assembly: AssemblyInformationalVersion("9.2.0")]
 
 #if !NETSTANDARD2_0
 [assembly: NUnit.Framework.Timeout(60000)]
